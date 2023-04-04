6bc6caa777ca07dd0192c74a2183d7a3f319e81e
Dilshan <tdm.dev2@gmail.com>
2023-04-03 23:24:17 -0700

    Basic google map screen (map working)

---
+++ lib/screens/map/user_map.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(7.8794508,80.70625), //sri lanka
+          zoom: 8.0,
+        ),
+      ),
+    );
+  }
+}
